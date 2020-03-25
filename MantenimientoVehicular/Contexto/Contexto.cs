1a661629d3198bacddd99c79a9807b43a61a228a
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-25 15:05:02 -0500

    Contexto Listo

--- MantenimientoVehicular/Contexto/Contexto.cs
+++ MantenimientoVehicular/Contexto/Contexto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MantenimientoVehicular.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +9,18 @@ namespace MantenimientoVehicular.Contexto
     public class Contexto :DbContext
     {
 
+        public DbSet<Articulo> articulo { get; set; }
+        public DbSet<Cliente> cliente { get; set; }
+        public DbSet<EntradaArticulo> entraArticulo { get; set; }
+        public DbSet<Mantenimiento> mantenimiento { get; set; }
+        public DbSet<PedidoProveedores> PedidoProveedores { get; set; }
+        public DbSet<Proveedores> proveedores { get; set; }
+        public DbSet<Usuario> usuario { get; set; }
+        public DbSet<Vehiculo> vehiculo  { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@" DataSource = MantenimientoDB.db");
+        }
     }
 }
