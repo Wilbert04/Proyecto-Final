f677996b84740c96aea6ec3da2b570ec63bafc78
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-29 13:18:34 -0500

    Creando Registro detalle pedido proveedor

---
+++ Proyecto-MantenimientoVehicular/UI/Registros/DetallePedidosProveedor.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_MantenimientoVehicular.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for DetallePedidosProveedor.xaml
+    /// </summary>
+    public partial class DetallePedidosProveedor : Window
+    {
+        public DetallePedidosProveedor()
+        {
+            InitializeComponent();
+        }
+    }
+}
